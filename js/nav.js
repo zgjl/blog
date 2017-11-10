6c84fc1e6ea2eaf904e24a57a877bde8cf866e3a
zgjl <jlzcyx@126.com>
2017-11-11 01:06:31 +0800

    v1.0

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+
+$(function () {
+	$("#li1").click(function () {
+		$("#section1").show();
+		$("#section1").siblings().hide();
+	});
+
+	$("#li2").click(function () {
+		$("#section2").show();
+		$("#section2").siblings().hide();
+	});
+
+	$("#li3").click(function () {
+		$("#section3").show();
+		$("#section3").siblings().hide();
+	})
+
+	$("#li4").click(function () {
+		$("#section4").show();
+		$("#section4").siblings().hide();
+	})
+	
+})
