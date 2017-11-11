255fc34dade32d055dbe4fb99fb0374847d6f3c8
zgjl <jlzcyx@126.com>
2017-11-11 13:24:50 +0800

    v1.0

---
+++ js/more.js
@@ -0,0 +1,19 @@
+$(".more").click(function () {
+    var p = $(this).next(".content").find("p");
+    var str = p.css("overflow");
+    if (str.indexOf("hidden") >= 0) {
+        p.css({
+            "line-height": "1.5em",
+            "height": "auto",
+            "overflow": "visible"
+        })
+        $(this).text("收起")
+    } else {
+        p.css({
+            "line-height": "1.5em",
+            "height": "9em",
+            "overflow": "hidden"
+        })
+        $(this).text("全文")
+    }
+})
--- js/nav.js
+++ js/nav.js
@@ -1,22 +1,22 @@
 
 $(function () {
 	$("#li1").click(function () {
-		$("#section1").show();
+		$("#section1").slideDown(1000);
 		$("#section1").siblings().hide();
 	});
 
 	$("#li2").click(function () {
-		$("#section2").show();
+		$("#section2").slideDown(1000);
 		$("#section2").siblings().hide();
 	});
 
 	$("#li3").click(function () {
-		$("#section3").show();
+		$("#section3").slideDown(1000);
 		$("#section3").siblings().hide();
 	})
 
 	$("#li4").click(function () {
-		$("#section4").show();
+		$("#section4").slideDown(1000);
 		$("#section4").siblings().hide();
 	})
 	
