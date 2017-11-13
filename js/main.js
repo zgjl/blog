c778f5a0309c76b3997d014671e837f5472274c4
zgjl <jlzcyx@126.com>
2017-11-14 00:08:22 +0800

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 $(function () {
-
+// 导航栏效果
 	$("#li1").click(function () {
-		$(this).css("font-size","16px")
-		$(this).siblings().css("font-size","14px")
+		$(this).css("font-size", "16px")
+		$(this).siblings().css("font-size", "14px")
 		$("#section1").slideDown(1000);
 		$("#section1").siblings().hide();
 	});
 
 	$("#li2").click(function () {
-		$(this).css("font-size","16px")
-		$(this).siblings().css("font-size","14px")
+		$(this).css("font-size", "16px")
+		$(this).siblings().css("font-size", "14px")
 		$("#section2").slideDown(1000);
 		$("#section2").siblings().hide();
 	});
 
 	$("#li3").click(function () {
-		$(this).css("font-size","16px")
-		$(this).siblings().css("font-size","14px")
+		$(this).css("font-size", "16px")
+		$(this).siblings().css("font-size", "14px")
 		$("#section3").slideDown(1000);
 		$("#section3").siblings().hide();
 	})
 
 	$("#li4").click(function () {
-		$(this).css("font-size","16px")
-		$(this).siblings().css("font-size","14px")
+		$(this).css("font-size", "16px")
+		$(this).siblings().css("font-size", "14px")
 		$("#section4").slideDown(1000);
 		$("#section4").siblings().hide();
 	})
 
 	$("#li5").click(function () {
-		$(this).css("font-size","16px")
-		$(this).siblings().css("font-size","14px")
+		$(this).css("font-size", "16px")
+		$(this).siblings().css("font-size", "14px")
 		$("#section5").slideDown(1000);
 		$("#section5").siblings().hide();
 	})
-
+// 夜间模式切换
 	$(".switch-open").click(function () {
 		$(this).addClass("active");
 		$(".switch-close").removeClass("active");
@@ -55,10 +55,9 @@ $(function () {
 		$("#section3 .content a").css("color", "rgb(41, 164, 180)")
 		$(".more").css("color", "rgb(97, 137, 182)")
 	})
-	
+// 加载更多效果
 	var num2 = 1;
 	$(".loadhtml2").click(function () {
-
 		if (num2 == 8) {
 			$(this).html("我是有底线的")
 		} else {
@@ -71,36 +70,15 @@ $(function () {
 					$(".box").css("background-color", "rgb(120, 120, 120)")
 					$(".box").css("color", "rgb(255, 255, 255)")
 					$(".content p").css("color", "rgb(255, 255, 255)")
+					$(".more").css("color", "rgb(255, 255, 255)")
 				}
 			});
 			num2++;
 		}
 	})
 
-	// var num3 = 1;
-	// $(".loadhtml3").click(function () {
-
-	// 	if (num3 == 4) {
-	// 		$(this).html("我是有底线的")
-	// 	} else {
-	// 		var li = document.createElement("li");
-	// 		li.className = "box";
-	// 		var ul = $(this).prev();
-	// 		ul.append(li);
-	// 		ul.find("li").last().load("./html3/" + num3 + ".html", function () {
-	// 			if ($(".switch-open").hasClass("active")) {
-	// 				$(".box").css("background-color", "rgb(120, 120, 120)")
-	// 				$(".box").css("color", "rgb(255, 255, 255)")
-	// 				$(".content p").css("color", "rgb(255, 255, 255)")
-	// 			}
-	// 		});
-	// 		num3++;
-	// 	}
-	// })
-
 	var num4 = 1;
 	$(".loadhtml4").click(function () {
-
 		if (num4 == 4) {
 			$(this).html("我是有底线的")
 		} else {
@@ -113,14 +91,14 @@ $(function () {
 					$(".box").css("background-color", "rgb(120, 120, 120)")
 					$(".box").css("color", "rgb(255, 255, 255)")
 					$(".content p").css("color", "rgb(255, 255, 255)")
+					$(".more").css("color", "rgb(255, 255, 255)")
 				}
 			});
 			num4++;
 		}
 	})
-
-
-	$(".more").click(function () {
+// 全文收起效果，注意：此处使用$(document).on来为文档中的每个全文绑定点击事件	
+	$(document).on("click", ".more", function () {
 		var p = $(this).next(".content").find("p");
 		var str = p.css("overflow");
 		if (str.indexOf("hidden") >= 0) {
