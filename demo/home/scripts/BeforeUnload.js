634609f49dcd402c99cd5a5d52248bb502ac374b
zgjl <jlzcyx@126.com>
2017-11-13 11:50:41 +0800

    v1.0

--- demo/home/scripts/BeforeUnload.js
+++
@@ -1,6 +0,0 @@
-EventUtil.addHandler(window, "beforeunload", function(event){
-    event = EventUtil.getEvent(event);
-    var message = "您真的确定离开此页面吗？";
-    event.returnValue = message;
-    return message;
-});
--- demo/瀑布流/json对象数组.js
+++
@@ -1,6 +0,0 @@
-var a= new Object();
-a.data=[];
-a.data[0]="{ 'src': '1.jpg' }";
-alert(a.data[0]);
-alert(a);
-
