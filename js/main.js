de48993d7aedb3b75db4a94e1a25d83e323b0e81
zgjl <jlzcyx@126.com>
2017-11-16 23:34:07 +0800

    v1.5

--- js/main.js
+++ js/main.js
@@ -1,202 +1,202 @@
 $(function () {
 
-	var total2 = 11; //前端知识,网页数量
-	var total4 = 5; //文摘笔记,网页数量
+    var total2 = 11; //前端知识,网页数量
+    var total4 = 5; //文摘笔记,网页数量
 
-	var num2 = total2 - 3;
-	! function () {
-		for (var i = total2; i > num2; i--) {
-			var li = document.createElement("li");
-			li.className = "box";
-			var ul = $("#section2 ul");
-			ul.append(li);
-			ul.find("li").last().load("./html2/" + i + ".html", function () {
-				if ($(".switch-open").hasClass("active")) {
-					$(".box").css("background-color", "rgb(120, 120, 120)")
-					$(".box").css("color", "rgb(255, 255, 255)")
-					$(".content p").css("color", "rgb(255, 255, 255)")
-					$(".more").css("color", "rgb(255, 255, 255)")
-				}
-			});
-		}
-	}();
-	// 加载更多效果
-	$(".loadhtml2").click(function () {
-		if (num2 == 0) {
-			$(this).html("我是有底线的")
-		} else {
-			var li = document.createElement("li");
-			li.className = "box";
-			var ul = $(this).prev();
-			ul.append(li);
-			ul.find("li").last().load("./html2/" + num2 + ".html", function () {
-				if ($(".switch-open").hasClass("active")) {
-					$(".box").css("background-color", "rgb(120, 120, 120)")
-					$(".box").css("color", "rgb(255, 255, 255)")
-					$(".content p").css("color", "rgb(255, 255, 255)")
-					$(".more").css("color", "rgb(255, 255, 255)")
-				}
-			});
-			num2--;
-		}
-	})
+    var num2 = total2 - 3;
+    ! function () {
+        for (var i = total2; i > num2; i--) {
+            var li = document.createElement("li");
+            li.className = "box";
+            var ul = $("#section2 ul");
+            ul.append(li);
+            ul.find("li").last().load("./html2/" + i + ".html", function () {
+                if ($(".switch-open").hasClass("active")) {
+                    $(".box").css("background-color", "rgb(120, 120, 120)")
+                    $(".box").css("color", "rgb(255, 255, 255)")
+                    $(".content p").css("color", "rgb(255, 255, 255)")
+                    $(".more").css("color", "rgb(255, 255, 255)")
+                }
+            });
+        }
+    }();
+    // 加载更多效果
+    $(".loadhtml2").click(function () {
+        if (num2 == 0) {
+            $(this).html("我是有底线的")
+        } else {
+            var li = document.createElement("li");
+            li.className = "box";
+            var ul = $(this).prev();
+            ul.append(li);
+            ul.find("li").last().load("./html2/" + num2 + ".html", function () {
+                if ($(".switch-open").hasClass("active")) {
+                    $(".box").css("background-color", "rgb(120, 120, 120)")
+                    $(".box").css("color", "rgb(255, 255, 255)")
+                    $(".content p").css("color", "rgb(255, 255, 255)")
+                    $(".more").css("color", "rgb(255, 255, 255)")
+                }
+            });
+            num2--;
+        }
+    })
 
-	var num4 = total4 - 3;
-	! function () {
-		for (var i = total4; i > num4; i--) {
-			var li = document.createElement("li");
-			li.className = "box";
-			var ul = $("#section4 ul");
-			ul.append(li);
-			ul.find("li").last().load("./html4/" + i + ".html", function () {
-				if ($(".switch-open").hasClass("active")) {
-					$(".box").css("background-color", "rgb(120, 120, 120)")
-					$(".box").css("color", "rgb(255, 255, 255)")
-					$(".content p").css("color", "rgb(255, 255, 255)")
-					$(".more").css("color", "rgb(255, 255, 255)")
-				}
-			});
-		}
+    var num4 = total4 - 3;
+    ! function () {
+        for (var i = total4; i > num4; i--) {
+            var li = document.createElement("li");
+            li.className = "box";
+            var ul = $("#section4 ul");
+            ul.append(li);
+            ul.find("li").last().load("./html4/" + i + ".html", function () {
+                if ($(".switch-open").hasClass("active")) {
+                    $(".box").css("background-color", "rgb(120, 120, 120)")
+                    $(".box").css("color", "rgb(255, 255, 255)")
+                    $(".content p").css("color", "rgb(255, 255, 255)")
+                    $(".more").css("color", "rgb(255, 255, 255)")
+                }
+            });
+        }
 
-	}();
-	$(".loadhtml4").click(function () {
-		if (num4 == 0) {
-			$(this).html("我是有底线的")
-		} else {
-			var li = document.createElement("li");
-			li.className = "box";
-			var ul = $(this).prev();
-			ul.append(li);
-			ul.find("li").last().load("./html4/" + num4 + ".html", function () {
-				if ($(".switch-open").hasClass("active")) {
-					$(".box").css("background-color", "rgb(120, 120, 120)")
-					$(".box").css("color", "rgb(255, 255, 255)")
-					$(".content p").css("color", "rgb(255, 255, 255)")
-					$(".more").css("color", "rgb(255, 255, 255)")
-				}
-			});
-			num4--;
-		}
-	})
+    }();
+    $(".loadhtml4").click(function () {
+        if (num4 == 0) {
+            $(this).html("我是有底线的")
+        } else {
+            var li = document.createElement("li");
+            li.className = "box";
+            var ul = $(this).prev();
+            ul.append(li);
+            ul.find("li").last().load("./html4/" + num4 + ".html", function () {
+                if ($(".switch-open").hasClass("active")) {
+                    $(".box").css("background-color", "rgb(120, 120, 120)")
+                    $(".box").css("color", "rgb(255, 255, 255)")
+                    $(".content p").css("color", "rgb(255, 255, 255)")
+                    $(".more").css("color", "rgb(255, 255, 255)")
+                }
+            });
+            num4--;
+        }
+    })
 
-	// 导航栏效果
-	function liClick(selector, obj) {
-		var speed = $(selector).outerHeight() / 2;
-		obj.css("font-size", "16px")
-		obj.siblings().css("font-size", "14px")
-		$(selector).slideDown(speed);
-		$(selector).siblings().hide();
-	}
+    // 导航栏效果
+    function liClick(selector, obj) {
+        var speed = $(selector).outerHeight() / 2;
+        obj.css("font-size", "16px")
+        obj.siblings().css("font-size", "14px")
+        $(selector).slideDown(speed);
+        $(selector).siblings().hide();
+    }
 
-	$("#li1").click(function () {
-		obj = $(this)
-		liClick("#section1", obj)
-	});
-	$("#li2").click(function () {
-		obj = $(this)
-		liClick("#section2", obj)
-	});
-	$("#li3").click(function () {
-		obj = $(this)
-		liClick("#section3", obj)
-	});
-	$("#li4").click(function () {
-		obj = $(this)
-		liClick("#section4", obj)
-	});
-	$("#li5").click(function () {
-		obj = $(this)
-		liClick("#section5", obj)
-	});
+    $("#li1").click(function () {
+        obj = $(this)
+        liClick("#section1", obj)
+    });
+    $("#li2").click(function () {
+        obj = $(this)
+        liClick("#section2", obj)
+    });
+    $("#li3").click(function () {
+        obj = $(this)
+        liClick("#section3", obj)
+    });
+    $("#li4").click(function () {
+        obj = $(this)
+        liClick("#section4", obj)
+    });
+    $("#li5").click(function () {
+        obj = $(this)
+        liClick("#section5", obj)
+    });
 
-	// 夜间模式自动，晚上18点以后早上6点之前自动进入夜间模式
-	var dt1 = new Date();
-	var hour1 = dt1.getHours();
+    // 夜间模式自动，晚上18点以后早上6点之前自动进入夜间模式
+    var dt1 = new Date();
+    var hour1 = dt1.getHours();
 
-	if (!(hour1 >= 6 && hour1 <= 17)) {
-		$(".switch-open").addClass("active");
-		$(".switch-close").removeClass("active");
-		$("body").css("background-color", "rgb(63, 63, 63)");
-		$(".box").css("background-color", "rgb(120, 120, 120)");
-		$(".box").css("color", "rgb(255, 255, 255)");
-		$(".content p").css("color", "rgb(255, 255, 255)");
-		$("#section3 .content a").css("color", "rgb(255, 255, 255)");
-		$(".more").css("color", "rgb(255, 255, 255)");
-	}
-	// 夜间模式切换
-	$(".switch-open").click(function () {
-		$(this).addClass("active");
-		$(".switch-close").removeClass("active");
-		$("body").css("background-color", "rgb(63, 63, 63)");
-		$(".box").css("background-color", "rgb(120, 120, 120)");
-		$(".box").css("color", "rgb(255, 255, 255)");
-		$(".content p").css("color", "rgb(255, 255, 255)");
-		$("#section3 .content a").css("color", "rgb(255, 255, 255)");
-		$(".more").css("color", "rgb(255, 255, 255)");
-	})
+    if (!(hour1 >= 6 && hour1 <= 17)) {
+        $(".switch-open").addClass("active");
+        $(".switch-close").removeClass("active");
+        $("body").css("background-color", "rgb(63, 63, 63)");
+        $(".box").css("background-color", "rgb(120, 120, 120)");
+        $(".box").css("color", "rgb(255, 255, 255)");
+        $(".content p").css("color", "rgb(255, 255, 255)");
+        $("#section3 .content a").css("color", "rgb(255, 255, 255)");
+        $(".more").css("color", "rgb(255, 255, 255)");
+    }
+    // 夜间模式切换
+    $(".switch-open").click(function () {
+        $(this).addClass("active");
+        $(".switch-close").removeClass("active");
+        $("body").css("background-color", "rgb(63, 63, 63)");
+        $(".box").css("background-color", "rgb(120, 120, 120)");
+        $(".box").css("color", "rgb(255, 255, 255)");
+        $(".content p").css("color", "rgb(255, 255, 255)");
+        $("#section3 .content a").css("color", "rgb(255, 255, 255)");
+        $(".more").css("color", "rgb(255, 255, 255)");
+    })
 
-	$(".switch-close").click(function () {
-		$(this).addClass("active");
-		$(".switch-open").removeClass("active");
-		$("body").css("background-color", "rgb(150, 150, 150)");
-		$(".box").css("background-color", "rgb(255, 255, 240)");
-		$(".box").css("color", "rgba(0, 0, 0, 0.8)");
-		$(".content p").css("color", "rgba(0, 0, 0, 0.8)");
-		$("#section3 .content a").css("color", "rgb(41, 164, 180)");
-		$(".more").css("color", "rgb(97, 137, 182)");
-	})
+    $(".switch-close").click(function () {
+        $(this).addClass("active");
+        $(".switch-open").removeClass("active");
+        $("body").css("background-color", "rgb(150, 150, 150)");
+        $(".box").css("background-color", "rgb(255, 255, 240)");
+        $(".box").css("color", "rgba(0, 0, 0, 0.8)");
+        $(".content p").css("color", "rgba(0, 0, 0, 0.8)");
+        $("#section3 .content a").css("color", "rgb(41, 164, 180)");
+        $(".more").css("color", "rgb(97, 137, 182)");
+    })
 
-	// 全文收起效果，注意：此处使用$(document).on来为文档中的每个全文绑定点击事件	
-	$(document).on("click", ".more", function () {
-		var p = $(this).next(".content").find("p");
-		var str = p.css("overflow");
-		if (str.indexOf("hidden") >= 0) {
-			p.css({
-				"line-height": "1.5em",
-				"height": "auto",
-				"overflow": "visible"
-			})
-			$(this).text("收起")
-		} else {
-			p.css({
-				"line-height": "1.5em",
-				"height": "9em",
-				"overflow": "hidden"
-			})
-			$(this).text("全文")
-		}
-	})
+    // 全文收起效果，注意：此处使用$(document).on来为文档中的每个全文绑定点击事件	
+    $(document).on("click", ".more", function () {
+        var p = $(this).next(".content").find("p");
+        var str = p.css("overflow");
+        if (str.indexOf("hidden") >= 0) {
+            p.css({
+                "line-height": "1.5em",
+                "height": "auto",
+                "overflow": "visible"
+            })
+            $(this).text("收起")
+        } else {
+            p.css({
+                "line-height": "1.5em",
+                "height": "9em",
+                "overflow": "hidden"
+            })
+            $(this).text("全文")
+        }
+    })
 
-	$("#li1").mouseover(function () {
-		$(this).css("background-color", "rgba(199, 158, 254,0.7)");
-	}).mouseout(function () {
-		$(this).css("background-color", "rgb(199, 158, 254)");
-	});
-	$("#li2").mouseover(function () {
-		$(this).css("background-color", "rgba(254, 187, 80,0.7)");
-	}).mouseout(function () {
-		$(this).css("background-color", "rgb(254, 187, 80)");
-	});
-	$("#li3").mouseover(function () {
-		$(this).css("background-color", "rgba(193, 228, 222,0.7)");
-	}).mouseout(function () {
-		$(this).css("background-color", "rgb(193, 228, 222)");
-	});
-	$("#li4").mouseover(function () {
-		$(this).css("background-color", "rgba(246, 149, 129,0.7)");
-	}).mouseout(function () {
-		$(this).css("background-color", "rgb(246, 149, 129)");
-	});
-	$("#li5").mouseover(function () {
-		$(this).css("background-color", "rgba(63, 254, 211,0.7)");
-	}).mouseout(function () {
-		$(this).css("background-color", "rgb(63, 254, 211)");
-	});
+    $("#li1").mouseover(function () {
+        $(this).css("background-color", "rgba(199, 158, 254,0.7)");
+    }).mouseout(function () {
+        $(this).css("background-color", "rgb(199, 158, 254)");
+    });
+    $("#li2").mouseover(function () {
+        $(this).css("background-color", "rgba(254, 187, 80,0.7)");
+    }).mouseout(function () {
+        $(this).css("background-color", "rgb(254, 187, 80)");
+    });
+    $("#li3").mouseover(function () {
+        $(this).css("background-color", "rgba(193, 228, 222,0.7)");
+    }).mouseout(function () {
+        $(this).css("background-color", "rgb(193, 228, 222)");
+    });
+    $("#li4").mouseover(function () {
+        $(this).css("background-color", "rgba(246, 149, 129,0.7)");
+    }).mouseout(function () {
+        $(this).css("background-color", "rgb(246, 149, 129)");
+    });
+    $("#li5").mouseover(function () {
+        $(this).css("background-color", "rgba(63, 254, 211,0.7)");
+    }).mouseout(function () {
+        $(this).css("background-color", "rgb(63, 254, 211)");
+    });
 
 
 
-	$("#music audio").css({
-		"width": "140px",
-		"height": "50px"
-	})
+    $("#music audio").css({
+        "width": "140px",
+        "height": "50px"
+    })
 })
--- js/minor.js
+++ js/minor.js
@@ -1,103 +1,101 @@
 // 回到底部效果
-window.onload = function () {
-	var obtn = document.getElementById('totop');
-	var obtn1 = document.getElementById('btn-top');
-	//获取窗口文档显示区的高度
-	var clientHeight = document.documentElement.clientHeight || window.innerHeight;
-	var timer1 = null;
-	var isTop = true;
+var obtn = document.getElementById('totop');
+var obtn1 = document.getElementById('btn-top');
+//获取窗口文档显示区的高度
+var clientHeight = document.documentElement.clientHeight || window.innerHeight;
+var timer1 = null;
+var isTop = true;
 
-	window.onscroll = function () {
-		//获取滚动高度
-		var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+window.onscroll = function () {
+    //获取滚动高度
+    var osTop = document.documentElement.scrollTop || document.body.scrollTop;
 
-		if (osTop >= clientHeight) {
-			obtn1.style.display = "block";
-		} else {
-			obtn1.style.display = "none";
-		}
-		if (!isTop) {
-			clearInterval(timer1);
-		}
-		isTop = false;
+    if (osTop >= clientHeight) {
+        obtn1.style.display = "block";
+    } else {
+        obtn1.style.display = "none";
+    }
+    if (!isTop) {
+        clearInterval(timer1);
+    }
+    isTop = false;
 
-	}
+}
 
-	obtn.onclick = function () {
+obtn.onclick = function () {
 
-		//设置定时器
-		timer1 = setInterval(function () {
-			var osTop = document.documentElement.scrollTop || document.body.scrollTop;
-			var ispeed = Math.floor(-osTop / 6);
-			document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
-			isTop = true;
-			// console.log(osTop - ispeed);
-			if (osTop === 0) {
-				clearInterval(timer1);
-			}
-		}, 30);
-	}
+    //设置定时器
+    timer1 = setInterval(function () {
+        var osTop = document.documentElement.scrollTop || document.body.scrollTop;
+        var ispeed = Math.floor(-osTop / 6);
+        document.documentElement.scrollTop = document.body.scrollTop = osTop + ispeed;
+        isTop = true;
+        // console.log(osTop - ispeed);
+        if (osTop === 0) {
+            clearInterval(timer1);
+        }
+    }, 30);
 }
 
 // 时间格式化
 function formatDate(dt) {
-	if (!dt) {
-		var dt = new Date();
-	}
+    if (!dt) {
+        var dt = new Date();
+    }
 
-	var year = dt.getFullYear();
-	var month = dt.getMonth() + 1;
-	var date = dt.getDate();
-	var hour = dt.getHours();
-	var minute = dt.getMinutes();
-	var second = dt.getSeconds();
-	var day = dt.getDay();
-	var days = '';
-	switch (day) {
-		case 0:
-			days = "星期天";
-			break;
-		case 1:
-			days = "星期一";
-			break;
-		case 2:
-			days = "星期二";
-			break;
-		case 3:
-			days = "星期三";
-			break;
-		case 4:
-			days = "星期四";
-			break;
-		case 5:
-			days = "星期五";
-			break;
-		case 6:
-			days = "星期六";
-			break;
-	}
+    var year = dt.getFullYear();
+    var month = dt.getMonth() + 1;
+    var date = dt.getDate();
+    var hour = dt.getHours();
+    var minute = dt.getMinutes();
+    var second = dt.getSeconds();
+    var day = dt.getDay();
+    var days = '';
+    switch (day) {
+        case 0:
+            days = "星期天";
+            break;
+        case 1:
+            days = "星期一";
+            break;
+        case 2:
+            days = "星期二";
+            break;
+        case 3:
+            days = "星期三";
+            break;
+        case 4:
+            days = "星期四";
+            break;
+        case 5:
+            days = "星期五";
+            break;
+        case 6:
+            days = "星期六";
+            break;
+    }
 
-	if (month < 10) {
-		month = "0" + month;
-	}
-	if (date < 10) {
-		date = "0" + date;
-	}
-	if (hour < 10) {
-		hour = "0" + hour;
-	}
-	if (minute < 10) {
-		minute = "0" + minute;
-	}
-	if (second < 10) {
-		second = "0" + second;
-	}
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (date < 10) {
+        date = "0" + date;
+    }
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
 
-	return days+ " " +year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
+    return days + " " + year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
 }
 var text = document.getElementById("time")
 setInterval(function () {
-	text.innerHTML = formatDate(new Date());
+    text.innerHTML = formatDate(new Date());
 }, 1000);
 
 // var welcome = document.getElementById("welcome")
